c4f1ba0c6ee3fe976dcda7923251ef5243e2bb78
Lustter <lusnivitor@outlook.com>
2025-03-25 10:43:07 -0300

    .

---
+++ JS/Aula12 - Introdução ao JS/manipulandoHTML/script.js
@@ -0,0 +1,28 @@
+//Mudar a cor da tela de fundo
+window.document.body.style.background = "black"
+
+
+
+//Mudar a cor da letra
+window.document.body.style.color = "white"
+
+
+
+//Criando um elemento no HTML
+window.document.body.innerHTML += "<h1> Brasil x Argentina </h1>"
+
+
+//Exibindo um alerta
+window.alert("Olá mundo")
+
+
+//Exibindo uma tela de entrada de dados
+var continuar = window.prompt("Voce deseja continuar?")
+window.alert(continuar)
+
+
+//Exibindo uma tela de confimação
+continuar = window.confirm("Você deseja continuar?")
+window.alert(continuar)
+
+
---
+++ JS/Aula12 - Introdução ao JS/script.js
@@ -0,0 +1,24 @@
+//VAR e LET
+
+//Declarando VAR, VAR sempre é uma vartiavel global
+var texto = "Hello World"
+console.log(texto);
+//console.log == print()
+
+//Declarando LET, LET é sempre uma variavel local
+let nome = "Vitor Alexandre Lusni"
+console.log("nome: ", nome);
+
+
+//Atribuindo um novo valor
+nome = "Pedro"
+console.log("nome: ", nome);
+
+//Atribuindo um novo valor
+texto = "Olá, mundo"
+console.log(texto);
+
+//Constante - const
+const valorPI = Math.PI
+console.log("O valor de PI é igual a ", valorPI)
+
