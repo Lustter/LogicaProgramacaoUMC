ccceaf345d24d75e071d32e37b511458373f57fb
Lustter <lusnivitor@outlook.com>
2025-05-20 10:41:36 -0300

    .

---
+++ PHP/Aula19-introduçãoCRUD-1/CRUD/back/excluirCadastro.php
@@ -0,0 +1,32 @@
+<?php
+
+    if(isset($_POST["id"])) {
+        include("../conexao/conexao.php");
+
+        $id = $_POST["id"];
+
+        //Prepara a consulta SQL para excluir o cadastro
+        $sql = "DELETE FROM usuarios WHERE ID = ?";
+        $stmt = $conn->prepare($sql);
+
+        if($stmt) {
+            $stmt->bind_param("i", $id);
+            //Executa a querry
+            $stmt->execute();
+
+            //Redirecionar o usuário
+            header("Location: verificarCadastro.php");
+            $stmt->close();
+        }else{
+            echo "<div class='mensagem erro'>Erro consulta</div>";
+        }
+
+        $conn->close();
+    }
+
+
+
+
+
+
+?>
--- PHP/Aula19-introduçãoCRUD-1/CRUD/back/verificarCadastro.php
+++ PHP/Aula19-introduçãoCRUD-1/CRUD/back/verificarCadastro.php
@@ -60,12 +60,45 @@
 
                         //Armazenar as informações vindas do DB
                         $row = $resultado->fetch_assoc();
-                        echo var_dump($row);
+                        
+                        echo "
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>Sobrenome</th>
+                                        <th>E-mail</th>
+                                        <th>Excluir</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <td>{$row['ID']}</td>
+                                    <td>{$row['NOME']}</td>
+                                    <td>{$row['SOBRENOME']}</td>
+                                    <td>{$row['EMAIL']}</td>
+                                    <td>
+                                        <form action='excluirCadastro.php' method='post'>
+                                            <input type='hidden' name='id' value='{$row['ID']}'>
+                                            <input type='submit' name='btn-excluir' value='excluir'>
+                                        </form>
+                                    </td>
+                                </tbody>
+                            </table>
+                        ";
 
                     }else{
-                        echo "ELE NÃO EXISTE";
+                        echo "<div class='mensagem erro'> E-mail $email não encontrado! </div>";
                     }
+
+                    //Encerra a conexão
+                    $stmt->close();
+                }else {
+                    echo "<div class='mensagem erro'> Erro na consulta </div>";
                 }
+
+                //encerra a conexão com o banco de dados
+                $conn->close();
 
             }
 
